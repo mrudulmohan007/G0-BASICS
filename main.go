8b007652fbf89c79642ce8e7d062a8fd6b06d107
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-02-07 15:35:14 +0530

    array examples in go

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("hello world")
+	//eg1
+	fmt.Println("Array samples in golang")
+	// var arr1 [3]string
+	var arr1 = [3]string{"Coffee", "Tea", "Other"}
+	fmt.Println("arr1: ", arr1)
+
+	//eg2
+	arr2 := [3]string{"Bus", "car", "bike"}
+	fmt.Println("arr2: ", arr2)
+
+	//eg3
+	arr3 := arr2
+	fmt.Println("arr3 : ", arr3)
+
+	//eg4
+	fmt.Println("length of the arr3: ", len(arr3))
 }
