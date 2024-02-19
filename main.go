6f89661bdd4075b6dd854f8ee026abdba9ebb91c
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-02-19 23:37:51 +0530

    custom data type in go

--- main.go
+++ main.go
@@ -247,48 +247,59 @@ import "fmt"
 
 //CONDITION STATEMENTS  / BRANCHING
 
+// func main() {
+// 	fmt.Println("Branching/Condition Statements")
+// 	i := 5
+// 	//1.If Statements
+// 	if i < 6 {
+// 		fmt.Println("i<6")
+// 	} else if i > 6 {
+// 		fmt.Println("i>6")
+// 	} else {
+// 		fmt.Println("Failed")
+// 	}
+
+// 	//2.switch statements
+// 	//condition based switch
+// 	switch i {
+// 	case 3:
+// 		fmt.Println("Value is 3")
+// 	case 5:
+// 		fmt.Println("Value is 5")
+// 	default:
+// 		fmt.Println("Default value")
+// 	}
+// 	//logical switch
+// 	switch {
+// 	case i < 6:
+// 		fmt.Println("i<6")
+// 	case i > 6:
+// 		fmt.Println("i>6")
+// 	default:
+// 		fmt.Println("Default value")
+// 	}
+
+// 	//3.goto statement
+// 	c := 8
+// 	if c > 7 {
+// 		fmt.Println("c is > 7")
+// 		goto mylabel
+// 	}
+
+// mylabel:
+// 	if c > 7 {
+// 		fmt.Println("after label : c>7")
+// 	}
+
+// }
+
+//OOP IN GO
+
+type Number int //custom data type
+
 func main() {
-	fmt.Println("Branching/Condition Statements")
-	i := 5
-	//1.If Statements
-	if i < 6 {
-		fmt.Println("i<6")
-	} else if i > 6 {
-		fmt.Println("i>6")
-	} else {
-		fmt.Println("Failed")
-	}
-
-	//2.switch statements
-	//condition based switch
-	switch i {
-	case 3:
-		fmt.Println("Value is 3")
-	case 5:
-		fmt.Println("Value is 5")
-	default:
-		fmt.Println("Default value")
-	}
-	//logical switch
-	switch {
-	case i < 6:
-		fmt.Println("i<6")
-	case i > 6:
-		fmt.Println("i>6")
-	default:
-		fmt.Println("Default value")
-	}
-
-	//3.goto statement
-	c := 8
-	if c > 7 {
-		fmt.Println("c is > 7")
-		goto mylabel
-	}
-
-mylabel:
-	if c > 7 {
-		fmt.Println("after label : c>7")
-	}
-
+	fmt.Println("Oops demo in go")
+	var num Number
+	num = 10
+	fmt.Println(num)
 }
